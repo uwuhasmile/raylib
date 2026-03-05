c7ccab9a60575b326d7f513d47ae450328625c2b
uwuhasmile <uwuhasmile@proton.me>
2026-03-05 18:43:33 +0200

    Music loop point variables introduced

--- src/raylib.h
+++ src/raylib.h
@@ -493,6 +493,8 @@ typedef struct Music {
     AudioStream stream;         // Audio stream
     unsigned int frameCount;    // Total number of frames (considering channels)
     bool looping;               // Music looping enable
+    unsigned int loopStart;     // Music loop starting point
+    unsigned int loopEnd;       // Music loop ending point
 
     int ctxType;                // Type of music context (audio filetype)
     void *ctxData;              // Audio context data, depends on type
